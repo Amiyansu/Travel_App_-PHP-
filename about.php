134daf54039afbee020aae3a763a40457fdcfd7b
Amiyansu Patra <139028543+Amiyansu@users.noreply.github.com>
2024-07-09 23:10:10 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,244 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>about</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <section class="header">
+        <a href="home.php" class="logo">travel</a>
+
+        <nav class="navbar">
+            <a href="home.php">home</a>
+            <a href="about.php">about</a>
+            <a href="package.php">package</a>
+            <a href="book.php">book</a>
+        </nav>
+
+        <div id="menu-btn" class="fas fa-bars"></div>
+
+    </section>
+
+
+
+
+
+
+    <div class="heading" style="background: url(images/header-img-1.jpg) no-repeat;">
+        <h1>about us</h1>
+    </div>
+
+
+
+    <section class="about">
+        <div class="image">
+            <img src="images/about-img.jpg" alt="">
+        </div>
+
+        <div class="content">
+            <h3>why choose us?</h3>
+            <p>Honest prices – we have the best prices for tours, and a huge selection of destinations will not leave indifferent even avid tourist.</p>
+            <p>Reliable partners – We work with the best tour operators who over the years have proven their professionalism.</p>
+            <p>Quality Guarantee – We do not have to worry about paperwork necessary documents.</p>
+
+            <div class="icons-container">
+                <div class="icons">
+                    <i class="fas fa-map"></i>
+                    <span>top destinations</span>
+                </div>
+                <div class="icons">
+                    <i class="fas fa-hand-holding-usd"></i>
+                    <span>affordable price</span>
+                </div>
+                <div class="icons">
+                    <i class="fas fa-headset"></i>
+                    <span>24/7 guide service</span>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+<section class="reviews">
+    <h1 class="heading-title"> clients reviews </h1>
+    <div class="swiper reviews-slider">
+        <div class="swiper-wrapper">
+
+            <div class="swiper-slide slide">
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                </div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et dolore, voluptates sapiente ut illo enim voluptatem laudantium nulla architecto blanditiis vitae, quos a veritatis harum dicta, ratione porro iure possimus.</p>
+                <h3>Amiya Patra</h3>
+                <span>traveler</span>
+                <img src="images/pic-1.png" alt="">
+            </div>
+
+            <div class="swiper-slide slide">
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                </div>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum eaque accusantium necessitatibus culpa neque perspiciatis ipsa quaerat ea maxime in? Laboriosam quasi mollitia sit eos, ipsam dolores corporis nesciunt assumenda earum quia autem? Velit, quia animi sint odio rem neque?</p>
+                <h3>Amiya Patra</h3>
+                <span>traveler</span>
+                <img src="images/pic-1.png" alt="">
+            </div>
+
+
+            <div class="swiper-slide slide">
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                </div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et dolore, voluptates sapiente ut illo enim voluptatem laudantium nulla architecto blanditiis vitae, quos a veritatis harum dicta, ratione porro iure possimus.</p>
+                <h3>Amiya Patra</h3>
+                <span>traveler</span>
+                <img src="images/pic-1.png" alt="">
+            </div>
+
+
+            <div class="swiper-slide slide">
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                </div>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusantium nobis dolore, ut voluptatum vitae recusandae.</p>
+                <h3>Amiya Patra</h3>
+                <span>traveler</span>
+                <img src="images/pic-1.png" alt="">
+            </div>
+
+            <div class="swiper-slide slide">
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                </div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et dolore, voluptates sapiente ut illo enim voluptatem laudantium nulla architecto blanditiis vitae, quos a veritatis harum dicta, ratione porro iure possimus.</p>
+                <h3>Amiya Patra</h3>
+                <span>traveler</span>
+                <img src="images/pic-1.png" alt="">
+            </div>
+
+            <div class="swiper-slide slide">
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                </div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et dolore, voluptates sapiente ut illo enim voluptatem laudantium nulla.
+                <h3>Amiya Patra</h3>
+                <span>traveler</span>
+                <img src="images/pic-1.png" alt="">
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+
+
+
+
+
+
+
+
+<section class="footer">
+    <div class="box-container">
+        <div class="box">
+            <h3>quick links</h3>
+            <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+            <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+            <a href="package.php"> <i class="fas fa-angle-right"></i> package</a>
+            <a href="book.php"> <i class="fas fa-angle-right"></i> book</a>
+        </div>
+        <div class="box">
+            <h3>extra links</h3>
+            <a href="#"> <i class="fas fa-angle-right"></i> ask questions</a>
+            <a href="#"> <i class="fas fa-angle-right"></i> about us</a>
+            <a href="#"> <i class="fas fa-angle-right"></i> privacy policy</a>
+            <a href="#"> <i class="fas fa-angle-right"></i> terms of use</a>
+        </div>
+        <div class="box">
+            <h3>contact info</h3>
+            <a href="#"> <i class="fas fa-phone"></i> +91 7683 866 998 </a>
+            <a href="#"> <i class="fas fa-phone"></i> +91 9938 696 038 </a>
+            <a href="#"> <i class="fas fa-envelope"></i> patraamiyansu@gmail.com </a>
+            <a href="#"> <i class="fas fa-map"></i> balasore, india - 756002 </a>
+        </div>
+        <div class="box">
+            <h3>follow us</h3>
+            <a href="#"> <i class="fab fa-facebook-f"></i> facebook</a>
+            <a href="#"> <i class="fab fa-twitter"></i> twitter</a>
+            <a href="#"> <i class="fab fa-instagram"></i> instagram</a>
+            <a href="#"> <i class="fab fa-linkedin"></i> linkedin</a>
+        </div>
+    </div>
+
+    <div class="credit">created by <span>Mr. Amiyansu Patra</span> | all right reserved!</div>
+</section>
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+<script>
+    // var swiper = new Swiper(".home-slider", {
+    //     loop: true,
+    //     navigation: {
+    //         nextEl: ".swiper-button-next",
+    //         prevEl: ".swiper-button-prev",
+    //     },
+    // });
+
+    var swiper = new Swiper(".reviews-slider", {
+        loop: true,
+        spaceBetween: 20,
+        autoHeight:true,
+        grabCursor:true,
+        breakpoints: {
+            640: {
+              slidesPerView: 1,
+            },
+            768: {
+              slidesPerView: 2,
+            },
+            1024: {
+              slidesPerView: 3,
+            },
+          },
+});
+</script>
+<script src="js/script.js"></script>
+</body>
+</html>
---
+++ home.php
@@ -0,0 +1,250 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>home</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <section class="header">
+        <a href="home.php" class="logo">travel</a>
+
+        <nav class="navbar">
+            <a href="home.php">home</a>
+            <a href="about.php">about</a>
+            <a href="package.php">package</a>
+            <a href="book.php">book</a>
+        </nav>
+
+        <div id="menu-btn" class="fas fa-bars"></div>
+
+    </section>
+
+
+<!-- swiper is inherit rom this website -->
+    <section class="home">
+        <div class="swiper home-slider">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide slide" style="background: url(images/home-slide-1.jpg) no-repeat" >
+                    <div class="content">
+                        <span>explore, discover, travel</span>
+                        <h3>travel the world with higher</h3>
+                        <a href="package.php" class="btn">discover more</a>
+                    </div>
+                </div>
+                <div class="swiper-slide slide" style="background: url(images/home-slide-2.jpg) no-repeat" >
+                    <div class="content">
+                        <span>explore, discover, travel</span>
+                        <h3>travel the new places</h3>
+                        <a href="package.php" class="btn">discover more</a>
+                    </div>
+                </div>
+                <div class="swiper-slide slide" style="background: url(images/home-slide-3.jpg) no-repeat" >
+                    <div class="content">
+                        <span>explore, discover, travel</span>
+                        <h3>discover the new places</h3>
+                        <a href="package.php" class="btn">discover more</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="swiper-button-next"></div>
+            <div class="swiper-button-prev"></div>
+        </div>
+    </section>
+    
+
+
+
+
+
+    <section class="services">
+        <h1 class="heading-title">our services</h1>
+        <div class="box-container">
+            <div class="box">
+                <img src="images/icon1.png" alt="">
+                <h3>adventure</h3>
+            </div>
+            <div class="box">
+                <img src="images/icon-2.png" alt="">
+                <h3>tour guide</h3>
+            </div>
+            <div class="box">
+                <img src="images/icon-3.png" alt="">
+                <h3>trekking</h3>
+            </div>
+            <div class="box">
+                <img src="images/icon-4.png" alt="">
+                <h3>camp fire</h3>
+            </div>
+            <div class="box">
+                <img src="images/icon-5.png" alt="">
+                <h3>off road</h3>
+            </div>
+            <div class="box">
+                <img src="images/icon-6.png" alt="">
+                <h3>camping</h3>
+            </div>
+        </div>
+    </section>
+
+
+
+
+
+
+<section class="home-about">
+    <div class="image">
+        <img src="images/about-img.jpg" alt="">
+    </div>
+
+    <div class="content">
+        <h3>about us</h3>
+        <p>
+        The World Travel Guide (WTG) is the flagship digital consumer brand within the Columbus Travel Media portfolio. A comprehensive guide to the world’s best travel destinations, its print heritage stretches back 30 years, with the online portal reaching its 19-year anniversary in 2018.</p>
+        <a href="about.php" class="btn">read more</a>
+    </div>
+</section>
+
+
+
+
+
+
+
+
+<section class="home-packages">
+    <h1 class="heading-title"> Our Packages</h1>
+
+    <div class="box-container">
+
+        <div class="box">
+            <div class="image">
+                <img src="images/img-1.jpg" alt="">
+            </div>
+            <div class="content">
+                <h3>adventure & tour</h3>
+                <p>Involving exploration with a certain degree of risk which may require special skills and physical exertion</p>
+                <a href="book.php" class="btn">Book Now</a>
+            </div>
+        </div>
+        <div class="box">
+            <div class="image">
+                <img src="images/img-2.jpg" alt="">
+            </div>
+            <div class="content">
+                <h3>adventure & tour</h3>
+                <p>Involving exploration with a certain degree of risk which may require special skills and physical exertion</p>
+                <a href="book.php" class="btn">Book Now</a>
+            </div>
+        </div>
+        <div class="box">
+            <div class="image">
+                <img src="images/img-3.jpg" alt="">
+            </div>
+            <div class="content">
+                <h3>adventure & tour</h3>
+                <p>Involving exploration with a certain degree of risk which may require special skills and physical exertion</p>
+                <a href="book.php" class="btn">Book Now</a>
+            </div>
+        </div>
+
+    </div>
+    <div class="load-more"><a href="package.php" class="btn">load more</a></div>
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<section class="home-offer">
+    <div class="content">
+        <h3>upto 50% off</h3>
+        <p>Amazing Travel Offers and Deals ; New User Offer. Register and Get Discount on First Bookings and apply coupon code- "EASEDAY" in the coupon section.</p>
+        <a href="book.php" class="btn">book now</a>
+    </div>
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<section class="footer">
+    <div class="box-container">
+        <div class="box">
+            <h3>quick links</h3>
+            <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+            <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+            <a href="package.php"> <i class="fas fa-angle-right"></i> package</a>
+            <a href="book.php"> <i class="fas fa-angle-right"></i> book</a>
+        </div>
+        <div class="box">
+            <h3>extra links</h3>
+            <a href="#"> <i class="fas fa-angle-right"></i> ask questions</a>
+            <a href="#"> <i class="fas fa-angle-right"></i> about us</a>
+            <a href="#"> <i class="fas fa-angle-right"></i> privacy policy</a>
+            <a href="#"> <i class="fas fa-angle-right"></i> terms of use</a>
+        </div>
+        <div class="box">
+            <h3>contact info</h3>
+            <a href="#"> <i class="fas fa-phone"></i> +91 7683 866 998 </a>
+            <a href="#"> <i class="fas fa-phone"></i> +91 9938 696 038 </a>
+            <a href="#"> <i class="fas fa-envelope"></i> patraamiyansu@gmail.com </a>
+            <a href="#"> <i class="fas fa-map"></i> balasore, india - 756002 </a>
+        </div>
+        <div class="box">
+            <h3>follow us</h3>
+            <a href="#"> <i class="fab fa-facebook-f"></i> facebook</a>
+            <a href="#"> <i class="fab fa-twitter"></i> twitter</a>
+            <a href="#"> <i class="fab fa-instagram"></i> instagram</a>
+            <a href="#"> <i class="fab fa-linkedin"></i> linkedin</a>
+        </div>
+    </div>
+
+    <div class="credit">created by <span>Mr. Amiyansu Patra</span> | all right reserved!</div>
+</section>
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+<script>
+    var swiper = new Swiper(".home-slider", {
+        loop: true,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
+</script>
+<script src="js/script.js"></script>
+</body>
+</html>
